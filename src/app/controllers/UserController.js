ae8a5fe017238f9ec55970d7162ec403de6a1f89
woneybranga <woney.branga@gmail.com>
2019-07-13 19:26:00 -0300

    optimized var use

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,7 +2,7 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+    const userExists = await User.findOne({ where: { email } });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
@@ -13,7 +13,10 @@ class UserController {
     } = await User.create(req.body);
 
     return res.json({
-      id, name, email, provider,
+      id,
+      name,
+      email,
+      provider,
     });
   }
 
