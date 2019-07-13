04f162d7ccabe4d627b0f9fdd9c54100e741c973
woneybranga <woney.branga@gmail.com>
2019-07-13 16:48:15 -0300

    added cryptography into password_hash fiield

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,7 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+import { isError } from 'util';
+
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +9,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +17,12 @@ class User extends Model {
         sequelize,
       },
     );
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+    return this;
   }
 }
 
